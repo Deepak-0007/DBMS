8bbf862fd20e288c97c4f6f550424beb9c184474
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2023-04-26 23:24:58 +0530

    dbms file of program

---
+++ dbms jdbc/c4119student.sql
@@ -0,0 +1,7 @@
+use c4119;
+Create table 4119Student (id int, Name varchar(20),age int);
+insert into 4119Student values(1,'sayan',23);
+insert into 4119Student values(1,'sayan',23);
+insert into 4119Student values(1,'sayan',23);
+insert into 4119Student values(1,'sayan',23);
+insert into 4119Student values(1,'sayan',23);
